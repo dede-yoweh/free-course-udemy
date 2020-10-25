419908148340bc688e113d05ce398615bf821037
oprah <mimidede222@gmail.com>
2020-10-24 17:48:31 -0700

    first-bracket-commit

---
+++ script.js
@@ -0,0 +1,42 @@
+/*var firstName = 'Johnyyyypppp'; 
+console.log(firstName);
+ 
+var lastName = 'Smith'
+console.log(lastName) */
+
+
+
+// type coercion
+var firstName = 'John';
+var age = 28;
+
+console.log(firstName + ' ' + age);
+
+// second example of type coercion
+var job, isMarried;
+job = 'teacher'
+isMarried = false;
+
+console.log(firstName + 'is a' + age + 'year old' + job + '. Is he married? ' + isMarried);
+
+// variable mutation (means to change the value of a variable)
+age = 'twenty eight';
+job = 'driver'
+
+
+// the alert will bring a pop up on the browser screen
+alert(firstName +  ' is a '  + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+// so with this a pop up will appear and you key in the last name. the last name then automatically adds to the first name since you have consoled both. but you bring a space before typing in the lastname on the browser pop up
+var lastName = prompt('What is his last Name');
+console.log(firstName + '' + lastName)
+
+
+
+
+
+
+
+
+
+
